679404e9b02d68607dfb574cc775db957e2d5aa6
assam0912 <1301446882@qq.com>
2025-10-30 20:41:59 +0800

    即将解决跨域问题

--- server/login-demo/src/main/java/org/example/interceptors/LoginInteceptor.java
+++ server/login-demo/src/main/java/org/example/interceptors/LoginInteceptor.java
@@ -18,6 +18,11 @@ public class LoginInteceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 放行 OPTIONS 预检请求
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
         // 令牌验证
         String token = request.getHeader("Authorization");
         // 验证token
